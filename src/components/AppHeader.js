0243161d216df388d97f26b5f3094b869f9d788a
Chotika-SE62 <59428867+Chotika-SE62@users.noreply.github.com>
2021-08-24 16:22:01 +0700

    Initial commit

---
+++ src/components/AppHeader.js
@@ -0,0 +1,11 @@
+import './AppHeader.css';
+
+function AppHeader() {
+    return (
+        <header className="app-header">
+            ดอกไม้ตามฤดูกาล
+        </header>
+    );
+}
+
+export default AppHeader;
---
+++ src/data/tattoos.js
@@ -0,0 +1,14 @@
+const tattoos = [
+    { title: "ดอกไม้ฤดูใบไม้ผลิ", thumbnailUrl: "/images/daisy-s.jpg", fullUrl: "/images/daisy-s.jpg" },
+    { title: "ดอกไม้ฤดูใบไม้ผลิ", thumbnailUrl: "/images/poppy-s.jpg", fullUrl: "/images/poppy-s.jpg" },
+    { title: "ดอกไม้ฤดูร้อน", thumbnailUrl: "/images/sunflower-h.jpg", fullUrl: "/images/sunflower-h.jpg" },
+    { title: "ดอกไม้ฤดูร้อน", thumbnailUrl: "/images/hydrangea-h.jpg", fullUrl: "/images/hydrangea-h.jpg" },
+    { title: "ดอกไม้ฤดูใบไม้ร่วง", thumbnailUrl: "/images/cosmos-f.jpg", fullUrl: "/images/cosmos-f.jpg" },
+    { title: "ดอกไม้ฤดูใบไม้ร่วง", thumbnailUrl: "/images/oxalis-f.jpg", fullUrl: "/images/oxalis-f.jpg" },
+    { title: "ดอกไม้ฤดูหนาว", thumbnailUrl: "/images/plum-w.jpg", fullUrl: "/images/plum-w.jpg" },
+    { title: "ดอกไม้ฤดูหนาว", thumbnailUrl: "/images/tsubaki-w.jpg", fullUrl: "/images/tsubaki-w.jpg" },
+    { title: "ดอกไม้ฤดูฝน", thumbnailUrl: "/images/dalia-r.jpg", fullUrl: "/images/dalia-r.jpg" },
+    { title: "ดอกไม้ฤดูฝน", thumbnailUrl: "/images/bougainvillea-r.jpg", fullUrl: "/images/bougainvillea-r.jpg" },
+];
+
+export default tattoos;
